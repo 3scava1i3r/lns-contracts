881d1ab3301a0e6bc6df422cb2b8f4a47e572c6b
3scava1i3r <tripathi.hritwik@gmail.com>
2022-08-26 18:33:48 +0530

    added most contracts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,21 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: "0.8.6",
+  networks: {
+    // evmos_mainnet: {
+    //   forking: {
+    //     url:"",
+    //   }
+    // },
+    // hardhat: {
+    //   forking: {
+    //     url: "https://grpc.bd.evmos.dev:9090",
+    //     // blockNumber: 1439000
+    //   }
+    // }  
+  },
+
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,79 @@
 import { ethers } from "hardhat";
+const namehash = require("eth-ens-namehash");
+const sha3 = require("web3-utils").sha3;
+const ZERO_HASH = "0x0000000000000000000000000000000000000000000000000000000000000000";
+
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  const ENSRegistry = await ethers.getContractFactory("ENSRegistry");
+  const BaseRegistrarImplementation = await ethers.getContractFactory("BaseRegistrarImplementation");
+  const evmosPriceOracle = await ethers.getContractFactory("evmosPriceOracle");
+  const PublicResolver = await ethers.getContractFactory("PublicResolver");
+  const ETHRegistrarController = await ethers.getContractFactory("ETHRegistrarController");
+  const ReverseRegistrar = await ethers.getContractFactory("ReverseRegistrar");
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
-  await lock.deployed();
+  // deploy ENSregistry
+  const ens = await ENSRegistry.deploy()
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  const ensInstance = await ens.deployed()
+  console.log("registry is deployed at " + `${ens.address}`)
+
+  // deploy BaseRegistrarImplementation
+
+  const BaseRegistrarImplementationInstance = await BaseRegistrarImplementation.deploy(ens.address,namehash.hash("light"))
+  const BRI = await BaseRegistrarImplementationInstance.deployed()
+  
+  console.log("BaseRegistrarImplementation is deployed at " + `${BaseRegistrarImplementationInstance.address}`)
+
+  const ll = await ethers.getSigners()
+  
+  //console.log(ll[0].address)
+  // add controller to BaseRegistrarImplementation
+  BRI.addController(ll[0].address) // not on mainnet comment the same
+
+  // set subnode owner to ENSRegistry
+  await ensInstance.setSubnodeOwner(ZERO_HASH, sha3("light"), BRI.address)
+  
+
+
+  const EvmosPOInstance = await evmosPriceOracle.deploy([0, 10, 100, 20, 10, 10, 10, 10])
+  const PO = await EvmosPOInstance.deployed()
+  console.log(PO.address + ` THIS IS PRICE ORACLE ADDRESS`)
+
+
+  // deploy PublicResolver
+  const PublicResolverInstance = await PublicResolver.deploy(ensInstance.address)
+  const PResolver = await PublicResolverInstance.deployed()
+  console.log(  `Public Resolver deployed at ${PResolver.address}`)
+
+  // deploy ReverseRegistrar
+  const ReverseRegistrarInstance = await ReverseRegistrar.deploy(ensInstance.address, PResolver.address)
+
+  const RR = await ReverseRegistrarInstance.deployed()
+  console.log(`ReverseRegistrarInstance deployed at ${RR.address}`)
+  
+
+  await ensInstance.setSubnodeOwner(ZERO_HASH, sha3('reverse'), ll[0].address, {
+    from: ll[0].address,
+  });
+
+  await ensInstance.setSubnodeOwner(
+    namehash.hash('reverse'),
+    sha3('addr'),
+    RR.address,
+    { from: ll[0].address }
+  );
+
+  // deploy ETHRegistrarController
+  const ETHRegistrarControllerInstance = await ETHRegistrarController.deploy(BRI.address,PO.address,60, 86400)
+
+  const ERCI = await ETHRegistrarControllerInstance.deployed()
+  console.log(`ETHRegistrarControllerInstanceAddress is ${ERCI.address}`)
+  // add controller to ENSRegistry
+  await BRI.addController(ERCI.address);
+  
 }
 
 // We recommend this pattern to be able to use async/await everywhere
